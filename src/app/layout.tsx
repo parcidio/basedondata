e968dda8a3a1013eb93a56f00629e353c58c8f37
parcidio <parcidioandre@gmail.com>
2024-01-11 23:55:50 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'basedondata',
+  description: 'BasedOnData is a webapp data host interactive graphs desgined from conducted research and other collected data',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
